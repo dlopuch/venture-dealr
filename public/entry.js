31a2db13f9963996b9413431545df3158bead74c
Dan Lopuch <dan@datahero.com>
2015-05-04 21:50:35 -0700

    starting the linting

--- public/entry.js
+++ public/entry.js
@@ -1,5 +1,5 @@
 window.onload = function() {
   document.getElementById('content_div').innerHTML = 'yo dan! ' + require('./content.js');
 
-  console.log('not so fast, FIXME: linting violation no semicolon')
+  console.log('not so fast, FIXME: linting violation no semicolon');
 };
