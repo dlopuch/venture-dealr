acc192e86cb62d812103844cdf796e4b5f1f3022
Dan Lopuch <dan@datahero.com>
2015-09-29 19:26:28 -0700

    Better positioning of first round labels

--- public/js/RoundChart.js
+++ public/js/RoundChart.js
@@ -167,7 +167,7 @@ class Chart {
     return this._components.xScale.rangeBand() * 0.8;
   }
 
-  getBarGutter() {
+  _getBarGutter() {
     return this._components.xScale.rangeBand() * 0.2 / 2;
   }
 
@@ -334,7 +334,7 @@ class Chart {
 
     // ----------------------
     // Now similar pattern for labels of new rounds
-    var firstRoundGlyphs = seriesG.selectAll('g.first-round-glyph')
+    var firstRoundLabels = seriesG.selectAll('g.first-round-label')
       .data(
         function(d) {
           // want to create one text element at the first non-null y value
@@ -343,13 +343,26 @@ class Chart {
         d => '' + d.id
       );
 
-    var firstRoundGlyphsEnter = firstRoundGlyphs.enter().append('g')
-      .classed('first-round-glyph', true);
-
-    var faGlyphs = firstRoundGlyphsEnter.append('text')
+    var firstRoundLabelsEnter = firstRoundLabels.enter().append('g')
+      .classed('first-round-label', true)
+      .style({
+        'opacity': 0,
+        'fill': d => d.yStake.shareClass === 'common' && !d.yStake.isOptionsPool ? '#eee' : '#222'
+      })
       .attr({
-        'text-anchor': 'middle',
-        'x': '-35px'
+        'transform': function(d) {
+          return 'translate(' +
+            (this._components.xScale(d.x) + this._getBarGutter() + 10) +
+            ', ' +
+            this._components.yScale(0) +
+            //(this._components.yScale(d.y0) - (d.y ? this._components.yHeightScale(d.y) / 2 : 0)) +
+            ')';
+        }.bind(this)
+      });
+
+    var faGlyphs = firstRoundLabelsEnter.append('text')
+      .attr({
+        'text-anchor': 'left',
       });
 
     faGlyphs.append('tspan')
@@ -366,34 +379,45 @@ class Chart {
     faGlyphs.append('tspan')
       .classed('fa', true)
       .attr({
-        'dominant-baseline' : 'middle',
-        'y': '0',
-        'dx': '3px'
+        'dominant-baseline': 'middle',
+        'y': '0'
       })
       .style({
-        'font-size'         : '40px'
+        'font-size': '40px'
       })
-      .html('&#xf183'); // .fa-male
+      .html(function(d) {
+        if (d.yStake.isOptionsPool || d.yStake.fromOptionsPool) {
+          return '&#xf1ae'; // .fa-child
+        } else if (d.yStake.investment) {
+          return '&#xf27e'; // .fa-black-tie
+        } else {
+          return Math.random() > 0.5 ? '&#xf183' : '&#xf182'; // .fa-male or .fa-female
+        }
 
-   var firstRoundGlyphsText = firstRoundGlyphsEnter.append('text')
-      .attr('text-anchor', 'left');
+      });
+
+   var firstRoundLabelsText = firstRoundLabelsEnter.append('text')
+      .attr({
+        'text-anchor': 'left',
+        'x': '55px'
+      });
 
 
-    firstRoundGlyphsText.append('tspan')
+    firstRoundLabelsText.append('tspan')
       .attr({
         'dominant-baseline': 'middle',
-        'dy': '-1em',
+        'dy': '-1.1em',
       })
       .text(function(d) {
         return d.yStake.name;
       });
 
-    firstRoundGlyphsText.append('tspan')
+    firstRoundLabelsText.append('tspan')
       .classed('series-value', true)
       .attr({
         'dominant-baseline': 'middle',
-        'dy': '1em',
-        'x': 0
+        'dy': '0.9em',
+        'x': '55px'
       })
       .style({
         'font-size': '20px',
@@ -401,18 +425,23 @@ class Chart {
       });
       // text filled in on update
 
-    firstRoundGlyphs
+    firstRoundLabels
+    .transition()
+      .duration(DEFAULT_TRANSITION_MS)
     .attr({
       'transform': function(d) {
         return 'translate(' +
-          (this._components.xScale(d.x) + this._getBarGutter() + 40 + 20) +
+          (this._components.xScale(d.x) + this._getBarGutter() + 10) +
           ', ' +
           (this._components.yScale(d.y0) - (d.y ? this._components.yHeightScale(d.y) / 2 : 0)) +
           ')';
       }.bind(this)
+    })
+    .style({
+      'opacity': d => d.n ? 1 : 0
     });
 
-    firstRoundGlyphs.selectAll('tspan.series-value')
+    firstRoundLabels.selectAll('tspan.series-value')
     .text(function() {
       // ignore the function d, that's stale because it was bound at create time.
       // Look at the g.first-round-glyph parent to get latest data
@@ -420,8 +449,8 @@ class Chart {
       return measure.yAxis.formatter(d.n) + ' equity';
     });
 
-    firstRoundGlyphs.exit().remove();
-    seriesG.exit().selectAll('g.first-round-glyph').remove();
+    firstRoundLabels.exit().remove();
+    seriesG.exit().selectAll('g.first-round-label').remove();
 
 
     // ----------------------
