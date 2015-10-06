caf7c9f93bbaad09d5e6bb3fa863fdff1adef117
Dan Lopuch <dan@datahero.com>
2015-10-05 19:47:20 -0700

    mock tracking code

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -4,7 +4,7 @@ exports.event = function(category, action, label, value, meta) {
   window.ga('send', 'event', category, action, label, value, meta);
 
   window.mixpanel.track(
-    (category || '') + (action || ''),
+    (category || '') + ' ' + (action || ''),
     _.merge({
       label: label,
       value: value,
