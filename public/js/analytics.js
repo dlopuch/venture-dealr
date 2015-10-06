26b445f010f4e44f7dd38f4725bf2d485e3a7ac5
Dan Lopuch <dan@datahero.com>
2015-10-05 19:37:46 -0700

    Adding analytics tracking of scenario activation

---
+++ public/js/analytics.js
@@ -0,0 +1,13 @@
+var _ = require('lodash');
+
+exports.event = function(category, action, label, value, meta) {
+  window.ga('send', 'event', category, action, label, value, meta);
+
+  window.mixpanel.track(
+    (category || '') + (action || ''),
+    _.merge({
+      label: label,
+      value: value,
+    }, (meta || {}))
+  );
+};
